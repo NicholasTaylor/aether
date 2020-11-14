cef366dc9b1aae1f0d9e45f6293030404d2f013f
Nicholas Taylor <nicholas_taylor@me.com>
2020-11-14 13:32:25 -0500

    Initial commit

---
+++ resources/views/static-homepage.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href={{ asset('css/bootstrap-reboot.min.css') }}>
+        <link rel="stylesheet" href="https://use.typekit.net/hkc3dnk.css">
+        <link rel="stylesheet" href={{ asset('css/homepage.css') }}>
+    </head>
+    <header>
+        <div id="logo">
+            <h1>
+                Aether_
+            </h1>
+        </div>
+        <nav id="nav-top">
+            <ul>
+                <li>
+                    Journal
+                </li>
+                <li>
+                    Lifehacks
+                </li>
+                <li>
+                    Code
+                </li>
+                <li>
+                    Writing
+                </li>
+                <li>
+                    Drawing
+                </li>
+                <li>
+                    Memes
+                </li>
+                <li>
+                    Gaming
+                </li>
+                <li>
+                    Miscellaneous
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <body>
+        <div id="container">
+            <article class="entry">
+                <div class="bg-screen">
+                </div>
+                <div class="bg-video">
+                    <video autoplay muted loop id="filename-here">
+                        <source src="{{ asset('images/manhattan-bridge.mp4') }}" type="video/mp4">
+                    </video>
+                </div>
+                <div class="entry-content">
+                    <h1>
+                        raw denim selvage kale chips pitchfork gochujang la croix
+                    </h1>
+                    <p>
+                        tattooed cred actually freegan drinking vinegar tumblr put a bird on it cardigan umami. Bitters neutra woke skateboard twee. Heirloom small batch tbh poutine mixtape.
+                    </p>
+                </div>
+            </article>
+            <article class="entry" style="background-image:url({{ asset('images/brookfield.jpg') }})">
+                <div class="bg-screen">
+                </div>
+                <div class="entry-content">
+                    <h1>
+                        pug taiyaki vinyl snackwave hammock butcher
+                    </h1>
+                    <p>
+                        Yuccie narwhal prism try-hard kombucha tbh DIY retro cred lumbersexual tousled live-edge tumblr.
+                    </p>
+                </div>
+            </article>
+        </div>
+    </body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,31 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.js('resources/js/app.js', 'public/js')
+    .postCss('resources/css/app.css', 'public/css', [
+        //
+    ])
+    .styles([
+    'resources/css/bootstrap-reboot.min.css'
+    ], 'public/css/bootstrap-reboot.min.css')
+    .copy(
+    'resources/fonts/',
+    'public/fonts/')
+    .styles(
+    'resources/css/gilroy.css',
+    'public/css/gilroy.css')
+    .sass(
+        'resources/scss/homepage.scss',
+    'public/css');
+
+mix.browserSync('127.0.0.1:8000');
